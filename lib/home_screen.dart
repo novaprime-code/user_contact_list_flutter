ffb6347e000be0df3c761d0b0623927fe5b64d2f
novaprime <novaprime143@gmail.com>
2022-11-29 22:49:05 +0545

    data fetched from the

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,20 +1,112 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:user_contact/model/user.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  // State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  getUserData() async {
+    // var url = "https://jsonplaceholder.typicode.com/users";
+// method 1
+    var url = Uri.https('jsonplaceholder.typicode.com', 'users');
+    final response = await http.get(url);
+    // var url = "https://jsonplaceholder.typicode.com/users";
+    // var response = await http.get();
+    // method 3
+    // var response = await http.get(Uri.http('jsonplaceholder.typicode.com', 'users', {'_limit': '2'}));
+    // var data = jsonDecode(response.body);
+    // print(data);
+
+    // final String name;
+    // final String username;
+    // final String email;
+    // final Address address;
+    // final String phone;
+    // final String website;
+    // final Company company;
+    var jsonData = jsonDecode(response.body);
+    List<User> users = [];
+    for (var u in jsonData) {
+      User user = User(u["id"], u["name"], u["username"], u["email"],
+          u["phone"], u["website"]);
+      // User user = User(u["name"], u["email"], u["phone"], u["username"],
+      //     u["address"], u["company"], u["website"]);
+      users.add(user);
+    }
+    print(users.length);
+    return users;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text('Welcome to the HomeScreen'),
+      // body: const Center(
+      //   child: ElevatedButton(
+      //     child: Text('Click Me'),
+      //     onPressed: getUserData(),
+      //   ),
+      // ),
+      body: Card(
+        // shape: RoundedRectangleBorder(
+        //   side: BorderSide(color: Colors.green.shade300),
+        //   borderRadius: BorderRadius.circular(15.0),
+        // ),
+        child: FutureBuilder(
+          future: getUserData(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Center(
+                child: Text("Loading..."),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+
+                    shape: BeveledRectangleBorder(
+                      //<-- SEE HERE
+                      side: BorderSide(width: 2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundColor: const Color(0xff6ae792),
+                      child: Text(
+                        snapshot.data[index].id.toString(),
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    // title: Text(snapshot.data[index].name),
+                    title: Column(
+                      children: [
+                        Text(snapshot.data[index].name),
+                        Text(snapshot.data[index].username),
+                        Text(snapshot.data[index].email),
+                        Text(snapshot.data[index].phone),
+                        Text(snapshot.data[index].website),
+                      ],
+                    ),
+                    trailing: Icon(Icons.more_vert),
+                    // spacing: 20;
+                    // SizedBox(height: 10),
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -2,35 +2,46 @@ import 'address.dart';
 import 'company.dart';
 
 class User {
+  final int id;
   final String name;
   final String username;
   final String email;
-  final Address address;
+  // final Address address;
   final String phone;
   final String website;
-  final Company company;
+  // final Company company;
 
-  User({
-    required this.name,
-    required this.username,
-    required this.email,
-    required this.address,
-    required this.phone,
-    required this.website,
-    required this.company,
-  });
+  User(
+    this.id,
+    this.name,
+    this.username,
+    this.email,
+    // this.address,
+    this.phone,
+    this.website,
+    // this.company,
+  );
+  // User({
+  //   required this.name,
+  //   required this.username,
+  //   required this.email,
+  //   required this.address,
+  //   required this.phone,
+  //   required this.website,
+  //   required this.company,
+  // });
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      name: json['name'],
-      username: json['username'],
-      email: json['email'],
-      address: Address.fromJson(json['address']),
-      phone: json['phone'],
-      website: json['website'],
-      company: Company.fromJson(json['company']),
-    );
-  }
+  // factory User.fromJson(Map<String, dynamic> json) {
+  //   return User(
+  //     name: json['name'],
+  //     username: json['username'],
+  //     email: json['email'],
+  //     address: Address.fromJson(json['address']),
+  //     phone: json['phone'],
+  //     website: json['website'],
+  //     company: Company.fromJson(json['company']),
+  //   );
+  // }
 }
 
 
